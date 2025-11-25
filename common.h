d64c6c15c28fe5a29c1aa07fa6d485bf6d6f1c5c
Rishi K <Rishi.K@iiitb.ac.in>
2025-11-25 11:59:30 +0530

    Add new functions and color codes to common.h

--- common.h
+++ common.h
@@ -1,20 +1,35 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-// --- Functions from user.c ---
+//For colour coded terminal output
+#define RESET   "\x1b[0m"
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define WHITE   "\x1b[37m"
+#define BOLD    "\x1b[1m"
+
+// Functions from user.c
 void addUser();
 int login(char* username_out);
 void showLeaderboard();
 void update_reward_score(const char* username, int points_to_add);
 
-// --- Functions from tasks.c ---
+
+
+// Functions from tasks.c
 void create_new_tasks(const char* username);
 void view_tasks(const char* username);
 void update_task(const char* username);
 int complete_task(const char* username);
-void delete_task(const char* username); // Renamed for clarity
-void rearrange_tasks(const char* username); // Renamed for clarity
-void show_reminders(const char* username); // Renamed for clarity
-void clear_input_buffer(); // This utility is used everywhere
+void delete_task(const char* username); 
+void rearrange_tasks(const char* username); 
+void show_reminders(const char* username); 
+void clear_input_buffer(); 
+void write_status(const char* username);
 
-#endif // COMMON_H
+
+#endif
