4bae93e519c8f771ff264b8a3f69bd875e329b51
Rishi K <Rishi.K@iiitb.ac.in>
2025-11-25 12:02:01 +0530

    Enhance comments in sha256.h for clarity
    Added comments to clarify the origin and purpose of the SHA-256 implementation.

--- sha256.h
+++ sha256.h
@@ -1,7 +1,11 @@
 /*
  * Originally from: https://github.com/amosnier/sha-256
  * This is free and unencumbered software released into the public domain.
+ * Since Openssl is not easily available on Windows this has been used to hash the passwords and store it safely.
+ *  It is not a part of the project source code, and can be treated as a library for helping hashing functionality
  */
+
+
 #ifndef SHA256_H
 #define SHA256_H
 
